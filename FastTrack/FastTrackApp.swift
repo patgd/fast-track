9ecb0a8fa0c79aab5ea132b5ebad7908c363e4ac
pat <pat@pat-mbp.local>
2023-01-04 09:22:34 -1000

    Initialized project.

---
+++ FastTrack/FastTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastTrackApp.swift
+//  FastTrack
+//
+//  Created by pat on 1/4/23.
+//
+
+import SwiftUI
+
+@main
+struct FastTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
