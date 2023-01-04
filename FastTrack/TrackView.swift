d470f380c7ed429ef81cc1c100edef758c9497e0
pat <>
2023-01-04 12:51:15 -1000

    Abstracted each track's view into TrackView to improve legibility.

---
+++ FastTrack/TrackView.swift
@@ -0,0 +1,20 @@
+//
+//  TrackView.swift
+//  FastTrack
+//
+//  Created by pat on 1/4/23.
+//
+
+import SwiftUI
+
+struct TrackView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TrackView_Previews: PreviewProvider {
+    static var previews: some View {
+        TrackView()
+    }
+}
